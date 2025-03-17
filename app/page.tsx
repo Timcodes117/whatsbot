a65e4b9691cce4a501df988e68ce59b52e94f9dd
timothy <timcodes117@gmail.com>
2025-03-17 22:49:57 +0100

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,10 @@ import React from "react";
 const page = () => {
   return (
     <>
+    <main className="max-w-screen-2xl mx-auto  w-full">
       <MainHeader />
 
-      <section className="flex flex-row lg:items-center max-w-screen-2xl mx-auto min-h-[100vh] lg:px-24 px-5 relative overflow-hidden">
+      <section className="flex flex-row lg:items-center min-h-[100vh] lg:px-24 px-5 relative overflow-hidden">
         <div className="flex flex-col lg:justify-center relative **:transition-all **:duration-500">
           
           <div className="-z-10 scale-50 lg:scale-100 hero_blur2 left-0 top-5 origin-bottom-left" />
@@ -18,24 +19,24 @@ const page = () => {
             <div className=" w-[100px]  lg:w-[200px] h-[1px] invert-50 bg-black"></div>
           </div>
           <div className="flex flex-col">
-            <p className="text-[11vw] sm:text-[70px] xl:text-[80px] ">Your</p>
-            <p className="text-[11vw] sm:text-[70px] xl:text-[80px]  leading-12">
+            <p className="text-[11vw] sm:text-[70px] xl:text-[90px] ">Your</p>
+            <p className="text-[11vw] sm:text-[70px] xl:text-[90px]  leading-12">
               Smart{" "}
               <span className="font-medium" style={{ color: color_theme_enum.MAIN }}>WhatsApp</span>{" "}
             </p>
-            <p className="text-[11vw] sm:text-[70px] xl:text-[80px] ">Assistant</p>
+            <p className="text-[11vw] sm:text-[70px] xl:text-[90px] ">Assistant</p>
           </div>
           <br />
-          <div className="flex items-center justify-center w-fit scale-75 origin-top-left lg:scale-100">
-            <div className="hero_action_btn flex items-center flex-row gap-4 justify-center"></div>
-            <button className=" flex items-center  flex-row gap-4 justify-center absolute  ">
+          <div className="flex items-center justify-center active:opacity-50 w-fit scale-75 origin-top-left lg:scale-100">
+            <div className="hero_action_btn  flex items-center flex-row gap-4 justify-center"></div>
+            <button className=" flex items-center  flex-row gap-4 justify-center absolute cursor-pointer  ">
               <p className="text-[18px] font-semibold text-black">Get Started</p>
               <div className="w-[35px] h-[35px] bg-[url(/go.svg)] bg-center bg-contain bg-no-repeat" />
             </button>
           </div>
         </div>
 
-        <div className="w-[600px] h-[80vh] -z-10 absolute -right-56 md:right-0 -bottom-20 lg:bottom-0 flex flex-col justify-end scale-90 origin-bottom-right overflow-hidden">
+        <div className="w-[600px] h-fit max-h-[80vh] -z-10 absolute -right-56 md:right-0 -bottom-20 lg:bottom-0 flex flex-col justify-end scale-90 origin-bottom-right overflow-hidden">
           <div className="flex flex-col absolute bottom-5 w-full px-10  ">
           <div className="lg:flex flex-row justify-end hidden ">
           <ChatPop message="Get started" userTyper="you" />
@@ -47,12 +48,27 @@ const page = () => {
           <br />
           <br />
           </div>
-
-          <div className="w-[550px] bg-[url(/hero_img.svg)] h-[550px] bg-contain bg-center bg-no-repeat"></div>
+          <br />
+          <div className="w-[600px] bg-[url(/hero_img.svg)] h-[600px] bg-contain bg-center !bg-no-repeat"></div>
         </div>
       </section>
 
-      <section className="w-full h-[100vh]"></section>
+      <section className="w-full min-h-[600px] mt-20 bg-[#00ff4c56] rounded-4xl flex flex-col items-center p-10">
+      <br />
+      <br />
+          <h1 className=" text-[20vw] md:text-8xl  font-medium">Our</h1>
+          <h1 className=" text-[20vw] md:text-8xl  font-medium">Solution</h1>
+          <br />
+          <br />
+          <p className="text-base text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id earum fuga sunt nulla doloremque odio. Eveniet modi in libero cum recusandae atque, fuga animi aut! Itaque aperiam ad quis nam!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id earum fuga sunt nulla doloremque odio. Eveniet modi in libero cum recusandae atque, fuga animi aut! Itaque aperiam ad quis nam!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id earum fuga sunt nulla doloremque odio. Eveniet modi in libero cum recusandae atque, fuga animi aut! Itaque aperiam ad quis nam!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id earum fuga sunt nulla doloremque odio. Eveniet modi in libero cum recusandae atque, fuga animi aut! Itaque aperiam ad quis nam!
+          </p>
+          <br />
+
+      </section>
+    </main>
     </>
   );
 };
